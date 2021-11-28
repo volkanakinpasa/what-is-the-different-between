aa925eb757a14e930260af4b63da5986436dfacc
volkanakinpasa <volkanakintr@gmail.com>
2021-11-28 17:39:48 +0100

    update v2 to v3

--- background.js
+++ background.js
@@ -1,33 +1,36 @@
-if (!localStorage.getItem('install_time')) {
-
-    var now = new Date().getTime();
-    localStorage.setItem('install_time', now);
-    chrome.tabs.create({url: "options.html"});
-
-}
-
 var empty = ' ';
-var steamSearchText = 'What is the difference between';
+var searchText = 'What is the difference between';
 var searchUrl = 'http://www.google.com/search?q=';
 
-chrome.omnibox.onInputEntered.addListener(function(text, disposition) {
-    chrome.storage.sync.get({
-        searchUrl: searchUrl
-        }, function(items) {
-            if (items.searchUrl != null && items.searchUrl != undefined && items.searchUrl != '')
-                searchUrl = items.searchUrl;
+chrome.omnibox.onInputEntered.addListener(function (text, disposition) {
+  chrome.storage.sync.get(
+    {
+      searchUrl: searchUrl,
+    },
+    function (items) {
+      if (
+        items.searchUrl != null &&
+        items.searchUrl != undefined &&
+        items.searchUrl != ''
+      )
+        searchUrl = items.searchUrl;
 
-            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-                var currentTab = tabs[0];
-                text = steamSearchText + empty +  text;
-                var url = searchUrl + escape(text);
-                chrome.tabs.update(currentTab.id, {"url": url});   
-            });
-    });
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        var currentTab = tabs[0];
+        text = searchText + empty + text;
+        var url = searchUrl + escape(text);
+        chrome.tabs.update(currentTab.id, { url: url });
+      });
+    }
+  );
 });
 
-chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
-    suggest([
-        {content: text + empty, description: "<url><dim>" + steamSearchText + empty + "</dim><match>" + text + "</match></url>"}
-    ]);
+chrome.runtime.onInstalled.addListener(async (details) => {
+  if (details.reason == 'install') {
+    chrome.storage.local.get(['install_time'], function (result) {
+      var now = new Date().getTime();
+      chrome.storage.local.get({ install_time: now });
+      chrome.tabs.create({ url: 'options.html' });
+    });
+  }
 });
